a910c0f6b9221c4c6168fa6876b64d904e306256
Xabier Larrakoetxea <me@slok.dev>
2021-07-08 07:06:26 +0200

    Add label selector to Kubernetes controller
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- internal/app/kubecontroller/retriever.go
+++ internal/app/kubecontroller/retriever.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spotahome/kooper/v2/controller"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
@@ -14,18 +15,20 @@ import (
 
 // RetrieverKubernetesRepository is the service to manage k8s resources by the Kubernetes controller retrievers.
 type RetrieverKubernetesRepository interface {
-	ListPrometheusServiceLevels(ctx context.Context, ns string, labelSelector map[string]string) (*slothv1.PrometheusServiceLevelList, error)
-	WatchPrometheusServiceLevels(ctx context.Context, ns string, labelSelector map[string]string) (watch.Interface, error)
+	ListPrometheusServiceLevels(ctx context.Context, ns string, opts metav1.ListOptions) (*slothv1.PrometheusServiceLevelList, error)
+	WatchPrometheusServiceLevels(ctx context.Context, ns string, opts metav1.ListOptions) (watch.Interface, error)
 }
 
 // NewPrometheusServiceLevelsRetriver returns the retriever for Prometheus service levels events.
-func NewPrometheusServiceLevelsRetriver(ns string, repo RetrieverKubernetesRepository) controller.Retriever {
+func NewPrometheusServiceLevelsRetriver(ns string, labelSelector labels.Selector, repo RetrieverKubernetesRepository) controller.Retriever {
 	return controller.MustRetrieverFromListerWatcher(&cache.ListWatch{
 		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-			return repo.ListPrometheusServiceLevels(context.TODO(), ns, map[string]string{})
+			options.LabelSelector = labelSelector.String()
+			return repo.ListPrometheusServiceLevels(context.Background(), ns, options)
 		},
 		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-			return repo.WatchPrometheusServiceLevels(context.TODO(), ns, map[string]string{})
+			options.LabelSelector = labelSelector.String()
+			return repo.WatchPrometheusServiceLevels(context.Background(), ns, options)
 		},
 	})
 }

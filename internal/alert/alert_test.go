c41e2e3f9f03dcc33aa67dc42b16951946d1e9c5
Xabier Larrakoetxea <me@slok.dev>
2021-10-09 09:28:41 +0200

    Refactor how alert windows are implemented and calculated to allow using different full period windows like 28 day
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- internal/alert/alert_test.go
+++ internal/alert/alert_test.go
@@ -16,15 +16,6 @@ func TestGenerateMWMBAlerts(t *testing.T) {
 		expAlerts *alert.MWMBAlertGroup
 		expErr    bool
 	}{
-		"Generating alerts different to 30 day time window should fail.": {
-			slo: alert.SLO{
-				ID:         "test",
-				TimeWindow: 31 * 24 * time.Hour,
-				Objective:  99.9,
-			},
-			expErr: true,
-		},
-
 		"Generating a 30 day time window alerts should generate the alerts correctly.": {
 			slo: alert.SLO{
 				ID:         "test",
@@ -62,6 +53,49 @@ func TestGenerateMWMBAlerts(t *testing.T) {
 					ShortWindow:    6 * time.Hour,
 					LongWindow:     3 * 24 * time.Hour,
 					BurnRateFactor: 1,
+					ErrorBudget:    0.09999999999999432,
+					Severity:       alert.TicketAlertSeverity,
+				},
+			},
+		},
+
+		"Generating a 28 day time window alerts should generate the alerts correctly.": {
+			slo: alert.SLO{
+				ID:         "test",
+				TimeWindow: 28 * 24 * time.Hour,
+				Objective:  99.9,
+			},
+			expAlerts: &alert.MWMBAlertGroup{
+				PageQuick: alert.MWMBAlert{
+					ID:             "test-page-quick",
+					ShortWindow:    5 * time.Minute,
+					LongWindow:     1 * time.Hour,
+					BurnRateFactor: 13.44,
+					ErrorBudget:    0.09999999999999432,
+					Severity:       alert.PageAlertSeverity,
+				},
+				PageSlow: alert.MWMBAlert{
+					ID:             "test-page-slow",
+					ShortWindow:    30 * time.Minute,
+					LongWindow:     6 * time.Hour,
+					BurnRateFactor: 5.6000000000000005,
+					ErrorBudget:    0.09999999999999432,
+					Severity:       alert.PageAlertSeverity,
+				},
+
+				TicketQuick: alert.MWMBAlert{
+					ID:             "test-ticket-quick",
+					ShortWindow:    2 * time.Hour,
+					LongWindow:     1 * 24 * time.Hour,
+					BurnRateFactor: 2.8000000000000003,
+					ErrorBudget:    0.09999999999999432,
+					Severity:       alert.TicketAlertSeverity,
+				},
+				TicketSlow: alert.MWMBAlert{
+					ID:             "test-ticket-slow",
+					ShortWindow:    6 * time.Hour,
+					LongWindow:     3 * 24 * time.Hour,
+					BurnRateFactor: 0.9333333333333333,
 					ErrorBudget:    0.09999999999999432,
 					Severity:       alert.TicketAlertSeverity,
 				},

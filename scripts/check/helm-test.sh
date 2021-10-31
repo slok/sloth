512599a4187c102edf2550a4991a9402b62d673b
Asmund Tokheim <tokheim@outlook.com>
2021-10-31 16:33:02 +0100

    Create chart repo on github pages. Required changing helm structure
    Signed-off-by: Asmund Tokheim <tokheim@outlook.com>

--- scripts/check/helm-test.sh
+++ scripts/check/helm-test.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o nounset
 
-cd ./deploy/kubernetes/helm/tests
+cd ./deploy/kubernetes/helm/sloth/tests
 go test -race -coverprofile=.test_coverage.txt $(go list ./... | grep -v /test/integration )
 go tool cover -func=.test_coverage.txt | tail -n1 | awk '{print "Total test coverage: " $3}'

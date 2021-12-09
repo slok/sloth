f351ded3ae66bce52abff0ea0995447e767e2545
Xabier Larrakoetxea <me@slok.dev>
2021-12-09 09:23:30 +0100

    Regenerate kubernetes clients with Kubernetes v1.23 gen tools
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- scripts/kubegen.sh
+++ scripts/kubegen.sh
@@ -3,8 +3,8 @@
 set -o errexit
 set -o nounset
 
-IMAGE_CLI_GEN=quay.io/slok/kube-code-generator:v1.22.0
-IMAGE_CRD_GEN=quay.io/slok/kube-code-generator:v1.22.0
+IMAGE_CLI_GEN=quay.io/slok/kube-code-generator:v1.23.0
+IMAGE_CRD_GEN=quay.io/slok/kube-code-generator:v1.23.0
 ROOT_DIRECTORY=$(dirname "$(readlink -f "$0")")/../
 PROJECT_PACKAGE="github.com/slok/sloth"
 GEN_DIRECTORY="pkg/kubernetes/gen"
@@ -26,7 +26,7 @@ echo "Generating Kubernetes CRD manifests..."
 docker run -it --rm \
 	-v ${ROOT_DIRECTORY}:/src \
 	-e GO_PROJECT_ROOT=/src \
-	-e CRD_FLAG="crd:trivialVersions=true,allowDangerousTypes=true" \
+	-e CRD_FLAG="crd:crdVersions=v1,allowDangerousTypes=true" \
 	-e CRD_TYPES_PATH=/src/pkg/kubernetes/api \
 	-e CRD_OUT_PATH=/src/pkg/kubernetes/gen/crd \
 	${IMAGE_CRD_GEN} update-crd.sh
